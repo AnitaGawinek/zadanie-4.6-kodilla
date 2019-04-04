5848e9bad52218f719c3f73c6323400e50836879
Anita Gawinek <anitagawinek@gmail.com>
2019-04-04 14:41:12 +0200

    Second Commit

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,23 @@
 package com.kodilla;
 
 public class Calculator {
+    public double a = 100;
+    public double b = 200;
+
+    public void addNumbers() {
+        double result = a + b;
+        System.out.println("Your result is: " + result);
+    }
+    public void subtractNumbers() {
+        double result = a - b;
+        System.out.println("Your result is: " + result);
+    }
+    public void multiplyNumbers() {
+        double result = a * b;
+        System.out.println("Your result is: " + result);
+    }
+    public void divideNumbers() {
+        double result = a / b;
+        System.out.println("Your result is: " + result);
+    }
 }
--- src/main/java/com/kodilla/CalculatorApplication.java
+++ src/main/java/com/kodilla/CalculatorApplication.java
@@ -2,12 +2,17 @@ package com.kodilla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.lang.*;
+import java.util.*;
 
 @SpringBootApplication
 public class CalculatorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(CalculatorApplication.class, args);
+        Calculator calculator = new Calculator();
+        calculator.addNumbers();
+        calculator.subtractNumbers();
+        calculator.multiplyNumbers();
+        calculator.divideNumbers();
     }
-
 }
